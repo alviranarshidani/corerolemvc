a978e4ced035077014bc68d0de9df46258ef31f5
Alvira Narshidani <Alvira.Narshidani@GeorgianCollege.ca>
2020-03-22 01:42:29 -0400

    added Administration controller with constructor and readonly property

--- corerolemvc/Controllers/AdministrationController.cs
+++ corerolemvc/Controllers/AdministrationController.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace corerolemvc.Controllers
 {
     public class AdministrationController : Controller
     {
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
         public IActionResult Index()
         {
             return View();
