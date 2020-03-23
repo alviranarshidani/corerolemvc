8c43d804b2a4c6ba492bb481b35315e149932c5b
Alvira Narshidani <Alvira.Narshidani@GeorgianCollege.ca>
2020-03-23 13:59:16 -0400

    added User Manager Property and Action to Edit Role

--- corerolemvc/Controllers/AdministrationController.cs
+++ corerolemvc/Controllers/AdministrationController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using corerolemvc.Models;
+using corerolemvc.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,10 +12,12 @@ namespace corerolemvc.Controllers
     public class AdministrationController : Controller
     {
         private readonly RoleManager<IdentityRole> roleManager;
+        private readonly UserManager<IdentityUser> userManager;
 
-        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        public AdministrationController(RoleManager<IdentityRole> roleManager, UserManager<IdentityUser> userManager)
         {
             this.roleManager = roleManager;
+            this.userManager = userManager;
         }
         public IActionResult Index()
         {
@@ -58,6 +61,34 @@ namespace corerolemvc.Controllers
             var roles = roleManager.Roles;
 
             return View(roles);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditRoles(string id)
+        {
+            var role = await roleManager.FindByIdAsync(id);
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with Id = {id} cannot be found";
+                return View("Not Found");
+            }
+
+            var model = new EditRoleViewModel
+            {
+                Id = role.Id,
+                RoleName = role.Name
+
+            };
+
+            foreach (var user in userManager.Users)
+            {
+                if (await userManager.IsInRoleAsync(user,role.Name))
+                {
+                    model.Users.Add(user.UserName);
+                }
+            }
+            return View(model);
         }
     }
 }
--- corerolemvc/ViewModels/EditRoleViewModel.cs
+++ corerolemvc/ViewModels/EditRoleViewModel.cs
@@ -8,7 +8,7 @@ namespace corerolemvc.ViewModels
 {
     public class EditRoleViewModel
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
 
         [Required(ErrorMessage = "Role Name is required")]
         public string RoleName { get; set; }
